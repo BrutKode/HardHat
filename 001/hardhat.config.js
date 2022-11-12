dc38b480101dde48342da6f4f8626968309a95e7
BrutKode <usefulvloging@gmail.com>
2022-11-12 20:24:22 +0530

    Added hardhat toolbox

--- 001/hardhat.config.js
+++ 001/hardhat.config.js
@@ -1,6 +1,6 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -12,11 +12,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 module.exports = {
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: process.env.CMC_API,
+  },
   solidity: "0.8.8",
   networks: {
     goerli: {
       url: process.env.INFURA_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API
   }
 };
