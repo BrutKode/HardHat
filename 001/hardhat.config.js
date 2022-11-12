c65fa3e841247bf40d4b4bd1f772e61639f14046
BrutKode <usefulvloging@gmail.com>
2022-11-12 19:43:23 +0530

    Adding Goerli Deployment

--- 001/hardhat.config.js
+++ 001/hardhat.config.js
@@ -1,7 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.8",
+  networks: {
+    goerli: {
+      url: process.env.INFURA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
