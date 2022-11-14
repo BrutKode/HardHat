0e3879c2149e256c1cdb6d73a90a66ea59de5300
BrutKode <usefulvloging@gmail.com>
2022-11-14 15:55:39 +0530

    Resetting Project

--- 003/hardhat.config.js
+++ 003/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.8",
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.INFURA_API,
+      },
+    },
+  }
 };
