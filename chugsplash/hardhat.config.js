27d411123c0fad0191cb553ad26dec8e3bbcd205
BrutKode <usefulvloging@gmail.com>
2023-01-16 15:51:12 +0530

    Chug Config

--- chugsplash/hardhat.config.js
+++ chugsplash/hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@chugsplash/plugins");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          outputSelection: {
+            "*": {
+              "*": ['storageLayout'],
+            },
+          },
+        },
+      },
+    ]
+  }
 };
